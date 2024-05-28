b9aa59440519c6fd1fc4d4a6e9186c99e3fd3c28
Nikhil Nitin Ranjan <72209132+nikhilnitinranjan@users.noreply.github.com>
2024-05-28 08:05:27 +0530

    delete

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/partials/header/components";
-
+import { HeaderComponent } from "./component/partials/header/components";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     imports: [RouterOutlet, HeaderComponent]
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, HeaderComponent_1]
 })
 export class AppComponent {
   title = 'frontend';
